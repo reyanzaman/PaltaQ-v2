6506aeb8a5428d21c167ef11ba80a1af05848cdd
Admin <reyanzaman.rz@gmail.com>
2023-11-11 06:47:08 +0600

    else condition added to db config

--- app/app/settings.py
+++ app/app/settings.py
@@ -86,16 +86,16 @@ if 'test' in sys.argv:
             'PORT': '5432',
         }
     }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': os.environ.get('DB_HOST'),
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASS'),
+        }
     }
-}
 
 
 # Password validation
