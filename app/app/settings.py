0fb972b28c3b1e663295aa9488ba81fb19c970b4
Khandoker Ashik Uz Zaman <reyanzaman.rz@gmail.com>
2023-11-13 05:36:05 +0600

    added environment variables

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,12 +79,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DEFAULT_DB_HOST = 'db'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST', DEFAULT_DB_HOST),
+        'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
