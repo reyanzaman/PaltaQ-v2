1eaa8295e5f265c7d2b9b813ef42ea8941cdb64c
reyanzaman <reyanzaman.rz@gmail.com>
2025-01-18 20:00:12 +0600

    info update

--- app/pages/test/page.tsx
+++ app/pages/test/page.tsx
@@ -2,7 +2,7 @@ import { nunito } from "@/app/ui/fonts";
 import "@/app/ui/neomorphism.css";
 import type { Metadata } from "next";
 
-import TestComponent from "@/app/components/testcomponent";
+// import TestComponent from "@/app/components/testcomponent";
 
 export const metadata: Metadata = {
   title: "Test Area",
@@ -15,7 +15,7 @@ export default function Admin(props) {
     <div className={`${nunito.className} antialiased flex flex-row pt-[2em]`}>
 
       <div className="w-full max-w-full lg:pl-[8em] lg:mr-[1.5em] overflow-auto">
-        <TestComponent />
+        {/* <TestComponent /> */}
       </div>
 
     </div>
